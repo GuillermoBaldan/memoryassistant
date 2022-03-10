e38d37949159e892b763cbc123aec88958a43e7c
GuillermoBaldan <guillermobaldan@gmail.com>
2022-03-11 00:41:59 +0100

    load data function implemented!

---
+++ Functions Library/system.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+function loadData(url) {
+  //2 Cargamos los datos
+  let data = fs.readFileSync(url, "utf8");
+  //3 Convertimos los datos a un objeto
+  let json = JSON.parse(data);
+  //4 Devolvemos el objeto
+  return json;
+}
+
+export { loadData };
