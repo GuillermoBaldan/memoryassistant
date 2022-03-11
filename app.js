9a365c6afc8440c1e343b45ca42e97756e6da9d4
GuillermoBaldan <guillermobaldan@gmail.com>
2022-03-11 01:33:47 +0100

    Fixes #1

---
+++ app.js
@@ -0,0 +1,11 @@
+//1 Cargamos los datos
+const fs = require("fs");
+//const loadData = require("./Functions Library/system.js");
+
+//console.log(loadData("./database/1Pedro.json"));
+console.log("Hola");
+
+/* setTimeout(() => {
+  console.log("Hola");
+}, 1    0000);
+ */
