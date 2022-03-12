45a0eb1419a3e043e9948bb148108a357f28c9e2
GuillermoBaldan <guillermobaldan@gmail.com>
2022-03-12 03:56:52 +0100

    last changes

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const system = require("./Functions Library/system.js");
+let data = system.loadData("database/1Pedro.json");
 
-console.log(system.loadData("./database/1Pedro.json"));
-console.log("Hola");
+console.log("Estos son los datos guardados");
+console.log(data);
+console.log("Ahora guardamos el timestand de este momento en una variable");
+data = data.push({ lastMoment: new Date().getTime() });
+console.log("Ahora vemos lo que tenemos guardado en la variable");
+console.log(data);
+console.log("Ahora guardamos los datos en el archivo");
+system.saveData("database/1Pedro.json", data);
